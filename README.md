feecb3b36c70fd2322703f45801a787d3630a563
cohurin <neila@users.noreply.github.com>
2022-06-16 18:17:14 +0900

    template commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Sho's Nextjs template
+
+## Features
+- typescript
+- tailwindcss
+- ESLint configuration
+- prettier configuration
+- vscode settings
+
+## Instantiate
+First, install all dependencies.
+
+```zsh
+yarn install
+```
+
+That's it! Now run the development server:
+
+```zsh
+yarn dev
+```
+
+Off you go!
---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>SOME_TITLE</title>
+        <meta name='description' content='' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div></div>
+    </div>
+  )
+}
+
+export default Home
