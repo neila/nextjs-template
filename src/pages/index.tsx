44067fed2b6aeaae170d03c694d54473510a0d69
cohurin <neila@users.noreply.github.com>
2022-07-25 20:40:33 +0900

    template update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,19 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>SOME_TITLE</title>
-        <meta name='description' content='' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>SOME_TITLE</title>
+                <meta name='description' content='' />
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
 
-      <div></div>
-    </div>
-  )
+            <div></div>
+        </div>
+    )
 }
 
 export default Home
