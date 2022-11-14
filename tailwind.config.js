9aac629e630c0fa4399d55e5a15d117aab67727d
SHØ <neila@users.noreply.github.com>
2022-11-15 05:04:23 +0900

    setup tailwind boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
       mobile: '320px',
@@ -63,5 +65,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
